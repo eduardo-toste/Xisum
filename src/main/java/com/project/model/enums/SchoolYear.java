ffc1e50df75093398cebdfee6aead37ce80f5216
eduardo-toste <duciochetti@gmail.com>
2026-07-10 07:39:19 -0300

    feat: create school year enum

---
+++ src/main/java/com/project/model/enums/SchoolYear.java
@@ -0,0 +1,35 @@
+package com.project.model.enums;
+
+public enum SchoolYear {
+
+    SIXTH_GRADE(6, "6th Grade", "6º Ano"),
+    SEVENTH_GRADE(7, "7th Grade", "7º Ano"),
+    EIGHTH_GRADE(8, "8th Grade", "8º Ano"),
+    NINTH_GRADE(9, "9th Grade", "9º Ano"),
+    HIGH_SCHOOL_1(10, "High School 1st Year", "1º Ano Ensino Médio"),
+    HIGH_SCHOOL_2(11, "High School 2nd Year", "2º Ano Ensino Médio"),
+    HIGH_SCHOOL_3(12, "High School 3rd Year", "3º Ano Ensino Médio");
+
+    private final int value;
+    private final String englishLabel;
+    private final String portugueseLabel;
+
+    SchoolYear(int value, String englishLabel, String portugueseLabel) {
+        this.value = value;
+        this.englishLabel = englishLabel;
+        this.portugueseLabel = portugueseLabel;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getEnglishLabel() {
+        return englishLabel;
+    }
+
+    public String getPortugueseLabel() {
+        return portugueseLabel;
+    }
+
+}
