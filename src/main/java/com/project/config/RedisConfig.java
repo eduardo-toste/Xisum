0b08e2969339b68a42aec84dee0eadb2f6b0de0c
eduardo-toste <duciochetti@gmail.com>
2026-07-13 07:11:24 -0300

    config: create redis configuration class

---
+++ src/main/java/com/project/config/RedisConfig.java
@@ -0,0 +1,25 @@
+package com.project.config;
+
+import com.project.model.Room;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Room> roomRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Room> template = new RedisTemplate<>();
+
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        return template;
+    }
+
+}
