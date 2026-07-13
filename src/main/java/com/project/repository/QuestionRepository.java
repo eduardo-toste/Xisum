e598b83865e7cd75660b8768940e638097f6de29
eduardo-toste <duciochetti@gmail.com>
2026-07-13 07:38:39 -0300

    feat: create question repository class

---
+++ src/main/java/com/project/repository/QuestionRepository.java
@@ -0,0 +1,10 @@
+package com.project.repository;
+
+import com.project.controller.QuestionController;
+import com.project.model.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface QuestionRepository extends JpaRepository<Question, UUID> {
+}
