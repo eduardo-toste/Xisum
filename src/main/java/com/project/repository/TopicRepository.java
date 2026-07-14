c136f5a94ae23287b299d36b25a7c36c8caa05f6
eduardo-toste <duciochetti@gmail.com>
2026-07-14 07:35:49 -0300

    feat: create topic repository

--- src/main/java/com/project/repository/TopicRepository.java
+++ src/main/java/com/project/repository/TopicRepository.java
@@ -3,7 +3,11 @@ package com.project.repository;
 import com.project.model.Topic;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TopicRepository extends JpaRepository<Topic, UUID> {
+
+    Optional<Topic> findByName(String topicName);
+
 }
