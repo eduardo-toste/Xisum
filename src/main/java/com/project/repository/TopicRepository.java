5e4545d72ba45a2921b5e65148e78a4a2ae9bffa
eduardo-toste <duciochetti@gmail.com>
2026-07-16 22:05:34 -0300

    feat: implement find by school year and subject query method

--- src/main/java/com/project/repository/TopicRepository.java
+++ src/main/java/com/project/repository/TopicRepository.java
@@ -1,13 +1,18 @@
 package com.project.repository;
 
 import com.project.model.Topic;
+import com.project.model.enums.SchoolYear;
+import com.project.model.enums.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface TopicRepository extends JpaRepository<Topic, UUID> {
 
     Optional<Topic> findByName(String topicName);
+
+    List<Topic> findBySchoolYearAndSubject(SchoolYear schoolYear, Subject subject);
 
 }
