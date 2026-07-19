1e34e8a8f24d152c72ce0607e45064498aa82263
eduardo-toste <duciochetti@gmail.com>
2026-07-19 15:48:32 -0300

    feat: create player model class to represent a player

---
+++ src/main/java/com/project/model/Player.java
@@ -0,0 +1,15 @@
+package com.project.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Player {
+
+    private String nickname;
+    private String token;
+    private Integer score;
+
+
+}
