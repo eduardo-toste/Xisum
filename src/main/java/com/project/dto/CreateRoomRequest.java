94327aaea80c0f9f8e967085051d0a3059dadf50
eduardo-toste <duciochetti@gmail.com>
2026-07-19 15:41:39 -0300

    feat: add create room request dto

---
+++ src/main/java/com/project/dto/CreateRoomRequest.java
@@ -0,0 +1,17 @@
+package com.project.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.UUID;
+
+public record CreateRoomRequest(
+
+        @NotNull(message = "Topic ID is required")
+        UUID topicId,
+
+        @NotBlank(message = "Creator nickname is required")
+        String createNickname
+
+) {
+}
