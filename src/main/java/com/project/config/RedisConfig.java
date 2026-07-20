586223a4c60b4318bb26149f5b48e17f103649f2
eduardo-toste <duciochetti@gmail.com>
2026-07-20 07:43:29 -0300

    config: update redis config to use the time module as an object mapper

--- src/main/java/com/project/config/RedisConfig.java
+++ src/main/java/com/project/config/RedisConfig.java
@@ -1,5 +1,7 @@
 package com.project.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.project.model.Room;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,9 +17,12 @@ public class RedisConfig {
     public RedisTemplate<String, Room> roomRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Room> template = new RedisTemplate<>();
 
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+
         template.setConnectionFactory(redisConnectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
 
         return template;
     }
