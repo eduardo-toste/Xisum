76dc48b533173406ec40392130a87ef7c7ab85b1
eduardo-toste <duciochetti@gmail.com>
2026-07-20 07:25:34 -0300

    feat: add builder annotation to player class

--- src/main/java/com/project/model/Player.java
+++ src/main/java/com/project/model/Player.java
@@ -1,10 +1,12 @@
 package com.project.model;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class Player {
 
     private String nickname;
