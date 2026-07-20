6767f0e7abe21e04fbb05ad70fe7557675d8d605
eduardo-toste <duciochetti@gmail.com>
2026-07-20 19:16:08 -0300

    feat: create join room endpoint

--- src/main/java/com/project/controller/RoomController.java
+++ src/main/java/com/project/controller/RoomController.java
@@ -1,16 +1,15 @@
 package com.project.controller;
 
 import com.project.dto.CreateRoomRequest;
+import com.project.dto.JoinRoomRequest;
+import com.project.dto.JoinRoomResponse;
 import com.project.dto.RoomResponse;
 import com.project.service.RoomService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/rooms")
@@ -23,6 +22,12 @@ public class RoomController {
     public ResponseEntity<RoomResponse> createRoom(@RequestBody @Valid CreateRoomRequest request) {
         RoomResponse response = roomService.createRoom(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PostMapping("/join")
+    public ResponseEntity<JoinRoomResponse> joinRoom(@RequestBody @Valid JoinRoomRequest request) {
+        JoinRoomResponse response = roomService.joinRoom(request);
+        return ResponseEntity.ok(response);
     }
 
 }
