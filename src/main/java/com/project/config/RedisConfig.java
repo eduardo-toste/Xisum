49b2ae7aba640f45fb9a070e33969495f64204dd
eduardo-toste <duciochetti@gmail.com>
2026-07-20 19:15:12 -0300

    config: change value serializer on redis configurations

--- src/main/java/com/project/config/RedisConfig.java
+++ src/main/java/com/project/config/RedisConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -22,7 +22,7 @@ public class RedisConfig {
 
         template.setConnectionFactory(redisConnectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(objectMapper, Room.class));
 
         return template;
     }
