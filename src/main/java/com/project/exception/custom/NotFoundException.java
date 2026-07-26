8fdea7927785ec8ff2bd06d0a1c203b29effe9ed
eduardo-toste <duciochetti@gmail.com>
2026-07-26 17:21:50 -0300

    feat(exception): create not found exception to group not found exceptions

---
+++ src/main/java/com/project/exception/custom/NotFoundException.java
@@ -0,0 +1,9 @@
+package com.project.exception.custom;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+}
