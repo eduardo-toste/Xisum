d746883997503937f021293de478c2a583eddf0b
eduardo-toste <duciochetti@gmail.com>
2026-07-26 17:21:00 -0300

    feat(exception): create business exception to group business rule exceptions

---
+++ src/main/java/com/project/exception/custom/BusinessException.java
@@ -0,0 +1,9 @@
+package com.project.exception.custom;
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
+
+}
