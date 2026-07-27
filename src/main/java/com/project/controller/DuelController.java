d452e7b2533d22b79398e20ea3ed73581e6ebe8c
eduardo-toste <duciochetti@gmail.com>
2026-07-27 07:55:57 -0300

    feat: create get duel result endpoint

--- src/main/java/com/project/controller/DuelController.java
+++ src/main/java/com/project/controller/DuelController.java
@@ -1,6 +1,7 @@
 package com.project.controller;
 
-import com.project.dto.DuelAnswerRequest;
+import com.project.dto.duel.DuelAnswerRequest;
+import com.project.dto.duel.DuelResultResponse;
 import com.project.service.DuelService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,12 @@ public class DuelController {
     public ResponseEntity<Void> answerQuestion(@PathVariable String code, @RequestBody @Valid DuelAnswerRequest request) {
         duelService.answerQuestion(code, request);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{code}/result")
+    public ResponseEntity<DuelResultResponse> getDuelResult(@PathVariable String code) {
+        DuelResultResponse response = duelService.getResult(code);
+        return ResponseEntity.ok(response);
     }
 
 }
